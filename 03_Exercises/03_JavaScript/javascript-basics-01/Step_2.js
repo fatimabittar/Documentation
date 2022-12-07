68be2e2e1b0f319886d9bb5fc2ab2477d537959b
fatima <fatimabittar25@gmail.com>
2022-12-08 00:11:06 +0200

    Step2

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/Step_2.js
@@ -0,0 +1,5 @@
+var Name= 'Doe';
+var Surname='John';
+var City='New-York';
+//alert is a function that takes one string (), you turned all these variables into one string by concatinating thrm by using "+"
+alert('Name: '+Name +'\n'+'Surname: '+Surname+'\n'+'City: '+City);
