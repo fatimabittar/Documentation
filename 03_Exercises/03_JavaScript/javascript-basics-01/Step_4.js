bde362be3a0b2f68bfa53bc2cca968da5eceb5be
fatima <fatimabittar25@gmail.com>
2022-12-08 00:48:09 +0200

    step4

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/Step_4.js
@@ -0,0 +1,9 @@
+var name= 'name';
+var surname='surname';
+var city='city';
+
+name=prompt('please enter your name');
+surname=prompt('please enter your surname');
+city=prompt('please enter your city');
+
+alert("Name: "+name+"\n" +'Surname: '+surname+'\n'+'City: '+city);
