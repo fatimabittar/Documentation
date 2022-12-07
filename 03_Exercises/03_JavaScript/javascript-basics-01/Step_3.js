37a3c23c01cdd2eb112a39894befa913e7335a25
fatima <fatimabittar25@gmail.com>
2022-12-08 00:28:58 +0200

    Step3

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/Step_3.js
@@ -0,0 +1,6 @@
+var name="username";
+//there are three functions to open a dialog boxes (pop-up ) [Alert : just warning with Ok button],[Confirm: question with ok or cancel button (yes/no)],
+//[prompt: question with a field to fill with yes/no and it return the answer(so you need to assign it to a variable)]
+name = prompt('Please enter your name');
+alert('Hello '+name);
+
