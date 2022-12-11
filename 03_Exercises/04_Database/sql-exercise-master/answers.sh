eb41ce3a32a2b8d7f657e498d265d6ada603f99c
fatima <fatimabittar25@gmail.com>
2022-12-11 14:22:22 +0200

    basic queries

---
+++ 03_Exercises/04_Database/sql-exercise-master/answers.sh
@@ -0,0 +1,7 @@
+select * from students
+select * from students where age > 30 
+select * from students where gender='F' and age=30
+select points from students where name='Alex'  
+INSERT INTO students VALUES (7'Fatima',24,'M',500)
+update students set points =400 where name='Basma' 
+update students set points =points-100 where name='Alex' 
