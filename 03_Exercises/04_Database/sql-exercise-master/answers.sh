4f7e358cd7bb9275688e349582947701aaacd2f2
fatima <fatimabittar25@gmail.com>
2022-12-11 14:58:59 +0200

    Creating Table

--- 03_Exercises/04_Database/sql-exercise-master/answers.sh
+++ 03_Exercises/04_Database/sql-exercise-master/answers.sh
@@ -1,3 +1,4 @@
+# Basic Queries
 select * from students
 select * from students where age > 30 
 select * from students where gender='F' and age=30
@@ -5,3 +6,24 @@ select points from students where name='Alex'
 INSERT INTO students VALUES (7'Fatima',24,'M',500)
 update students set points =400 where name='Basma' 
 update students set points =points-100 where name='Alex' 
+
+#Creating Table
+CREATE TABLE graduates(
+    ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    Name TEXT Not Null Unique,
+    Age INTEGER,
+	Gender TEXT,
+	Gradutaion TEXT,
+	Points INTEGER
+);
+
+INSERT INTO graduates(ID,NAME,AGE,GENDER,POINTS)
+SELECT ID, Name, Age, Gender,Points
+FROM students
+WHERE name ='Layal'
+UPDATE graduates
+set Gradutaion='08/09/2018'
+where name='Layal'
+
+
+Delete from students where ID= 4
