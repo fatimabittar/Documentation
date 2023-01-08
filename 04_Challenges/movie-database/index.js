2f6213b3f4a5871cf9953df784d6939a29bcb6f7
fatima <fatimabittar25@gmail.com>
2023-01-08 15:57:42 +0200

    step 3

--- 04_Challenges/movie-database/index.js
+++ 04_Challenges/movie-database/index.js
@@ -5,6 +5,15 @@ app.get("/", (req, res) => {
     res.status(200).send("ok");
 });
 
+app.get("/test", (req, res) => {
+    res.json({status:200, message:"ok"});
+});
+
+app.get("/time", (req, res) => {
+    const date = new Date();
+    res.json({status:200, message: date.getHours() + ':' + date.getMinutes()});
+});
+
 app.listen(4000, () => {
     console.log("Listen on the port 4000...");
 });
